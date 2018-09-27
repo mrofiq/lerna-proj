070d26bcc21cac5ee4d8ed482b2f63e38be36b32
Muhammad Rofiq <muhammad.rofiq@bhinneka.com>
2018-09-27 17:49:23 +0700

    sass support

--- packages/cms-starfish/index.tsx
+++ packages/cms-starfish/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppRoute } from "./src/routes";
+import "./src/assets/test.sass";
 
 ReactDOM.render(<AppRoute />, document.getElementById("app"));
